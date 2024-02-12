d00a522c03eab84f3ddd9b5dc005cfe8912ac15f
hipolitoIV <80554014+hipolitoIV@users.noreply.github.com>
2024-02-12 13:16:18 -0600

    Implemented base class MediaProduct

--- Homework_3/src/products/MediaProduct.java
+++ Homework_3/src/products/MediaProduct.java
@@ -1,5 +1,15 @@
 package products;
 
 public class MediaProduct {
+    protected String title;
+    protected double price;
+    protected int year;
+    protected Genre genre;
 
+    public MediaProduct(String title, double price, int year, Genre genre) {
+        this.title = title;
+        this.price = price;
+        this.year = year;
+        this.genre = genre;
+    }
 }
