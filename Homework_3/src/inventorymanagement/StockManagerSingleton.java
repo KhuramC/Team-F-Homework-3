7f74caf75d11664242e2323ae817b586058d63e3
KhuramC <khuramchoudhry123@gmail.com>
2024-02-13 11:16:22 -0600

    Added Singleton functions to StockManagerSingleton
    added private constructor and getInstance function as well as the
    filepath and instance fields.

--- Homework_3/src/inventorymanagement/StockManagerSingleton.java
+++ Homework_3/src/inventorymanagement/StockManagerSingleton.java
@@ -2,8 +2,26 @@ package inventorymanagement;
 
 public class StockManagerSingleton {
 
-	public StockManagerSingleton() {
-		// TODO Auto-generated constructor stub
+	private static StockManagerSingleton instance = null;
+	
+	private static final String inventoryFilePath = "files/inventory.csv";
+	
+	/**
+	 * Private constructor to create single instance.
+	 */
+	private StockManagerSingleton() {
+	}
+	
+	
+	/**
+	 * Creates a single instance if it has not been created yet.
+	 * @return the instance of the StockManagerSingleton.
+	 */
+	public StockManagerSingleton getInstance() {
+		if (instance == null) {
+			instance = new StockManagerSingleton();
+		}
+		return instance;	
 	}
 
 }
