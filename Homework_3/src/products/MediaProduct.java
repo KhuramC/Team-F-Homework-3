6c42276cffe9f060408a394289d9fdb84ef8c5fd
KhuramC <khuramchoudhry123@gmail.com>
2024-02-13 13:46:10 -0600

    Added getters and setters for Media Product class.

--- Homework_3/src/products/MediaProduct.java
+++ Homework_3/src/products/MediaProduct.java
@@ -12,4 +12,36 @@ public class MediaProduct {
         this.year = year;
         this.genre = genre;
     }
+
+	public String getTitle() {
+		return title;
+	}
+
+	public void setTitle(String title) {
+		this.title = title;
+	}
+
+	public double getPrice() {
+		return price;
+	}
+
+	public void setPrice(double price) {
+		this.price = price;
+	}
+
+	public int getYear() {
+		return year;
+	}
+
+	public void setYear(int year) {
+		this.year = year;
+	}
+
+	public Genre getGenre() {
+		return genre;
+	}
+
+	public void setGenre(Genre genre) {
+		this.genre = genre;
+	}
 }
